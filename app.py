35ed33c34d9f137aa58aed4367697323b63e27ee
ylyu777 <114843981+ylyu777@users.noreply.github.com>
2023-04-13 13:34:27 -0400

    update

--- app.py
+++ app.py
@@ -1,44 +1,44 @@
 from flask import Flask, render_template
-
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length
+# from wtforms import StringField, PasswordField, SubmitField
+# from wtforms.validators import DataRequired, Length
+# from flask_wtf import FlaskForm
 
 app = Flask(__name__)
-##app.config['SQL_URI']
+app.config['SECRET_KEY'] = 'secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 db = SQLAlchemy(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
-
 
 ## database declarations
-class Item(db.Model):
-    i_id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
-    name = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.String(200))
-    seller = db.Column(db.Integer, db.ForeignKey('User.u_id'), nullable = False)
-    category_1 = db.Column(db.String(50), nullable=False)
-    category_2 = db.Column(db.String(50))
-    category_3 = db.Column(db.String(50))
-    price = db.Column(db.Double(2), nullable=False)
-    condition = db.Column(db.String(50))
-    listing_datetime = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    sold = db.Column(db.Boolean)
-    exchange_pref = db.Column(db.Integer) ## or string
-    payment_pref = db.Column(db.String)
-
 class User(db.Model):
     u_id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
     screen_name = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(25), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
     rating = db.Column(db.Integer)
+    
+class Item(db.Model):
+    i_id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(200))
+    seller = db.Column(db.Integer, nullable = False)
+    category_1 = db.Column(db.String(50), nullable=False)
+    category_2 = db.Column(db.String(50))
+    category_3 = db.Column(db.String(50))
+    price = db.Column(db.Float(2), nullable=False)
+    condition = db.Column(db.String(50))
+    listing_datetime = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    sold = db.Column(db.Boolean)
+    exchange_pref = db.Column(db.Integer) ## or string
+    payment_pref = db.Column(db.String)
+# db.ForeignKey('User.u_id'),
 
-class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=25)])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Login')
+
+# class LoginForm(FlaskForm):
+#     username = StringField('Username', validators=[DataRequired(), Length(min=4, max=25)])
+#     password = PasswordField('Password', validators=[DataRequired()])
+#     submit = SubmitField('Login')
     
 @app.route('/')
 def home():
@@ -66,4 +66,6 @@ def confirmation():
 
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
