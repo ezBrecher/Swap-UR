627b36e3a581e509ce5728215ed2d0cf9f31af93
Zeke Brecher <ebrecher@u.rochester.edu>
2023-04-19 20:16:47 -0400

    .

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from werkzeug.utils import secure_filename
 import os
 
 app = Flask(__name__)
+app.static_folder = 'static'
 app.config['SECRET_KEY'] = 'secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
